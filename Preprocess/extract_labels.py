0383eff1ee2a70cae8873e51c39a98644c648b63
Nisha Kumari <161752254+7h3s74r5@users.noreply.github.com>
2026-07-10 13:11:07 +0530

    Update extract_labels.py

--- Preprocess/extract_labels.py
+++ Preprocess/extract_labels.py
@@ -10,6 +10,19 @@ files = sorted([
     if x.endswith(".npy")
 ])
 
+# ==============================
+# SHAPE
+# ==============================
+shape_counter = Counter()
+
+for file in files:
+
+    arr = np.load(file)
+
+    shape_counter[arr.shape] += 1
+
+print(shape_counter)
+
 print("Total Files:", len(files))
 
 # ==============================
