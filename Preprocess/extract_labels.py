f3c468f97d02e0716ee1a9b772964f8988a7e129
Nisha Kumari <161752254+7h3s74r5@users.noreply.github.com>
2026-07-10 13:02:40 +0530

    Update extract_labels.py

--- Preprocess/extract_labels.py
+++ Preprocess/extract_labels.py
@@ -1,4 +1,20 @@
+# ==============================
+# NUMBER OF FILES
+# ==============================
+files = sorted([
+    os.path.join(
+        SAVE_DIR,
+        x
+    )
+    for x in os.listdir(SAVE_DIR)
+    if x.endswith(".npy")
+])
 
+print("Total Files:", len(files))
+
+# ==============================
+# LABELS EXTRACTION
+# ==============================
 labels = []
 
 for file in files:
